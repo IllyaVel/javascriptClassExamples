24bdae4f58a80110e3ac6fd39baf6fb75c927bcd
IllyaVel <45797738+IllyaVel@users.noreply.github.com>
2018-12-12 15:15:14 +0200

    Update arrowFunction.js

--- arrowFunction.js
+++ arrowFunction.js
@@ -1,1 +1,20 @@
-// Code'll upload here
+//Arrow function =>
+let _log = () => console.log('Working');
+_log();
+
+let add = (x,y) => x + y;
+console.log(add(10,2));
+
+let square = b => b * b;
+console.log(square(10));
+
+let sayHello = (name) => {
+    this.name = name;
+    return 'Hello dear ' + `${this.name}`;
+}
+console.log(sayHello('Illya'));
+
+let person = (name) => ({
+    name: 'without name'
+})
+console.log(person.name = 'Jack');
