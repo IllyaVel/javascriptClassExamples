b2c8d0c6b36de514b592fbf8a9d05caf2ab0c458
IllyaVel <45797738+IllyaVel@users.noreply.github.com>
2018-12-15 12:24:54 +0200

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,1 +1,18 @@
-// to do ...
+//Arrays
+var user = new Array(); // <-- Empty array
+var arr = []; // <-- Empty array
+
+user[0] = 'Jack';
+user[1] = 'Vika';
+user[4] = 'Albert';
+// console.log(user.length); //Output: 5;
+
+// user.forEach((el) => {
+//     console.log(el);
+// }); // Output: 'Jack','Vika','Albert'
+
+/*for(let i = 0; i < user.length; i++) {
+    console.log(user[i]);
+}*/ //Output: Jack,Vika,undefined,undefined,Albert
+
+// console.log(user[3]); Output: undefined
